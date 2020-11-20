d405032ac1e1e79dad6648086486d1acb3392c8f
srivelloBeamable <srivello@beamable.com>
2020-11-20 14:54:08 -0600

    Adding Examples. Temp location.

---
+++ client/Assets/Scripts/Runtime/Beamable/Features/Examples/StatsService/StatCodingExample.cs
@@ -0,0 +1,42 @@
+﻿using DisruptorBeam;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Beamable.Features.Examples.Stats
+{
+   public class StatCodingExample : MonoBehaviour
+   {
+      //  Unity Methods   ------------------------------
+      protected void Start()
+      {
+         DisruptorEngine.Instance.Then(de =>
+         {
+            UseStatCoding(de);
+         });
+      }
+
+      //  Other Methods   ------------------------------
+      private async void UseStatCoding(IDisruptorEngine de)
+      {
+         string statKey = "MyExampleStat";
+         string access = "public";
+         string domain = "client";
+         string type = "player";
+         long id = de.User.id;
+
+         // Set Value
+         Dictionary<string, string> setStats =
+            new Dictionary<string, string>() { { statKey, "99" } };
+
+         await de.Stats.SetStats(access, setStats);
+
+         // Get Value
+         Dictionary<string, string> getStats = 
+            await de.Stats.GetStats(domain, access, type, id);
+
+         string myExampleStatValue = "";
+         getStats.TryGetValue(statKey, out myExampleStatValue);
+         Debug.Log("myExampleStatValue: " + myExampleStatValue);
+      }
+   }
+}
